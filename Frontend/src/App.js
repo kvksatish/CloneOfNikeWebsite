4cc65d07b1afe5e2ba5d073eb60ab6d52ae0db1c
KVK Satish <102022392+kvksatish@users.noreply.github.com>
2022-11-29 22:24:01 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { AllRoutes } from "./allRoutes/AllRoutes";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  let loader = document.getElementById('loader');
+  console.log(process.env, "qwertyhj")
+  useEffect(() => {
+    setTimeout(() => {
+      loader.style.display = 'none';
+      setLoading(false);
+    }, 5000)
+  }, [])
+
+  // console.log("█▓▒­░⡷⠂Nike⠐⢾░▒▓█");
+  return (
+    <div>
+      {!loading && <AllRoutes />}
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/list.js
@@ -0,0 +1,230 @@
+
+let msg = [
+    {
+        "_id": "6368ae4f5fc9689fed959535",
+        "type": "men",
+        "title": "Nike Air Max TW",
+        "text": "Sustainable Materials",
+        "description": "So you're in love with the classic look of the '90s, but you've got a thing for today's fast-paced culture. Meet the Air Max TW. Inspired by the treasured franchise that brought Nike Air cushioning to the world and laid the foundation for the track-to-street aesthetic, its eye-catching design delivers a 1–2 punch of comfort and fashion. Ready to highlight any 'fit, its lightweight upper pairs angular and organic lines to create an entrancing haptic effect. The contrasting colourways make it easy to style. And if you're ready for the next step, the 5 windows underfoot deliver a modern edge to visible Air cushioning.",
+        "details": [
+            "New logo on the tongue",
+            "Rubber outsole",
+            "Colour Shown: Light Bone/Olive Grey/Summit White/Red Clay",
+            "Style: DQ3984-002",
+            "Country/Region of Origin: Indonesia"
+        ],
+        "rating": 4,
+        "images": "https://i.postimg.cc/pV7T91Bq/product6.png",
+        "price": 14995,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed959536",
+        "type": "men",
+        "title": "Midnight Navy",
+        "text": "LeBron 2",
+        "description": "Showcase your game in this 1:1 throwback and celebrate the King's first run with the red, white and blue. All-American colours honour the special moment in history—a young LeBron flashing his raw talent—while graphics on the collar and tongue keep the royal detailing you expect. Laser-etched graphics, ballistic fabric on the sides and crisp leather round off the look that shined on the world stage.",
+        "details": [
+            "Direct import by the individual customer",
+            "Country of Origin: Vietnam",
+            "Net Quantity: 1 Pair",
+            "SKU: DR0826-100"
+        ],
+        "rating": 4.8,
+        "images": "https://i.postimg.cc/VLWk7L8H/product8.png",
+        "price": 18395,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed959537",
+        "type": "men",
+        "title": "Eastside Golf",
+        "text": "Air Jordan 12",
+        "description": "Available 12/11 at 7:30 am Out of the clay, into the future. Partnering with Eastside Golf, we're bringing a modern aesthetic to the AJ-12. Transcending traditional golf attire, the design's premium leather and neutral colourway pair with Metallic Gold accents to add some flash to your swing. The Burnt Sunrise outsole has a 'mud-splatter' effect—a reminder that you can overcome adversity, no matter where a bad shot lands you. Eastside Golf branding, a special-edition hangtag and a custom shoe box add an authentic finish to your game. This is the next generation of golf. You ready?",
+        "details": [
+            "Nike Global Trading B.V. Singapore Branch",
+            "SKU: DV1758-108",
+            "Net Quantity: 1 Pair",
+            "Direct import by the individual customer",
+            "Country of Origin: China"
+        ],
+        "rating": 4.5,
+        "images": "https://i.postimg.cc/L6YJNMsc/product5.png",
+        "price": 18395,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed959538",
+        "type": "men",
+        "title": "Air Zoom Flight 95",
+        "text": "Black and Valerian Blue",
+        "description": "Have you ever had déjà shoe? Flash back to one of the first shoes to popularise Zoom Air technology and relive the hoops revolution. With its flowing lines and futuristic 'bug-eye' detailing, it's no dream—it's your ticket to turning heads. Black synthetic suede gets energised with flashes of Valerian Blue and Ale Brown, keeping the pulse in each step.",
+        "details": [
+            "Nike Global Trading B.V. Singapore Branch",
+            "Net Quantity: 1 Pair",
+            "SKU: DV6994-001",
+            "Direct import by the individual customer",
+            "Country of Origin: Viet Nam"
+        ],
+        "rating": 4,
+        "images": "https://i.postimg.cc/g0dyC0XX/product10.png",
+        "price": 14995,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed959539",
+        "type": "men",
+        "title": "Water",
+        "text": "Air Kukini",
+        "description": "Bridging casual and technical for over 20 years, the Air Kukini is proof that opposites attract. Carrying the ethos of Nike's clandestine Alpha Project and stamped with the division's iconic 5-dot logo, it's a futuristically wild design that pushes the envelope of what 'shoe' means. Reviving one of our most coveted colourways on the stretchy knit upper, an all-over water graphic in Coastal Blue takes a dip in early noughties style. The translucent support system adds a sporty finish to the hug-your-foot design. And visible Air cushioning adds the final touch to this boundary-pushing paradox of comfort.",
+        "details": [
+            "Nike Global Trading B.V. Singapore Branch",
+            "SKU: DV1894-400",
+            "Net Quantity: 1 Pair",
+            "Direct import by the individual customer",
+            "Country of Origin: Indonesia"
+        ],
+        "rating": 5,
+        "images": "https://i.postimg.cc/L6xPZq4L/product15.png",
+        "price": 12795,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed95953a",
+        "type": "men",
+        "title": "Nike Zoom Fly 5",
+        "text": "Men's Road Running Shoes",
+        "description": "Bridge the gap between your weekend training run and race day in a durable design that can be deployed not just at the starting line of your favourite race but in the days and months after your conquest. It offers comfort and reliability but with a propulsive sensation that'll help you feel fast and fresh. That kind of versatility is uncommon in the running arena. But who said you can't have it all?",
+        "details": [
+            "The upper is made from lightweight mesh ",
+            "Style: DM8968-800",
+            "Colour Shown: Total Orange/Bright Crimson/White/Black",
+            "Dynamic lace-up fit",
+            "Country/Region of Origin: Vietnam"
+        ],
+        "rating": 4.4,
+        "images": "https://i.postimg.cc/wBQg7Ngd/product16.png",
+        "price": 14995,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed95953b",
+        "type": "men",
+        "title": "Nike Air Force 1 Mid '07 LX",
+        "text": "Men's Shoes",
+        "description": "",
+        "details": [
+            "Padded collar",
+            "Colour Shown: Anthracite/Anthracite/Summit White/Black",
+            "Style: DV7585-001",
+            "Rubber outsole adds traction and durability.",
+            "Country/Region of Origin: China"
+        ],
+        "rating": 3.8,
+        "images": "https://i.postimg.cc/wMQ9sshN/product17.png",
+        "price": 11895,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed95953c",
+        "type": "men",
+        "title": "Air Jordan 7 Retro SE",
+        "text": "Men's Shoes",
+        "description": "Kick it back to '92 with this latest iteration of the AJ7, reviving the championship legacy for a new generation of sneakerheads. Come for the throwback style, stay all day for the premium construction and signature Air cushioning.",
+        "details": [
+            "Full-length Nike Air technology provides lightweight cushioning.",
+            "Rubber outsole gives your style traction.",
+            "Colour Shown: Vachetta Tan/Taxi/Dark Concord/Black",
+            "Style: DZ4729-200",
+            "Country/Region of Origin: China"
+        ],
+        "rating": 4,
+        "images": "https://i.postimg.cc/RZD4fmng/product18.png",
+        "price": 19295,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed95953d",
+        "type": "men",
+        "title": "Air Jordan XXXVI Low Luka PF",
+        "text": "Men's Basketball Shoes",
+        "description": "From the perimeter to the hoop, take the court with confidence. This is one of the lightest Air Jordan game shoes to date, with a minimal but durable leno-weave upper reinforced with tough plastic details. Meanwhile, Zoom Air cushioning delivers energy return and elite responsiveness. The game asks for everything—now you have the shoe to match.",
+        "details": [
+            "Light Feel, Big Energy",
+            "Colour Shown: Laser Blue/Electric Purple/Pink Prime/Electric Green",
+            "Style: DN4195-430",
+            "Multi-directional XDR outsole",
+            "Country/Region of Origin: Vietnam"
+        ],
+        "rating": 4.2,
+        "images": "https://i.postimg.cc/VkpPvF3T/product19.png",
+        "price": 16295,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed95953e",
+        "type": "men",
+        "title": "Nike Zoom Mercurial Superfly 9",
+        "text": "Firm-Ground Football Boot",
+        "description": "Kylian Mbappé's explosiveness, plus his twitchy change of direction, puts him firmly in the world class of finishers. With nods to his heritage, this boot is made to help you play just like KM—with blinding pace and purpose. We added a Zoom Air unit, made specifically for football, and grippy texture up top for exceptional touch, so you can dominate in the waning minutes of a match—when it matters most.",
+        "details": [
+            "For use on slightly wet, short-grass pitches",
+            "Style: DO9342-694",
+            "Cushioned insole",
+            "Colour Shown: Dark Beetroot/University Blue/Vivid Purple/Metallic Vivid Gold",
+            "Country/Region of Origin: China"
+        ],
+        "rating": 5,
+        "images": "https://i.postimg.cc/L5HrwQ1q/product20.png",
+        "price": 23995,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed95953f",
+        "type": "women",
+        "title": "Nike React Ace Tour",
+        "text": "Women's Golf Shoe",
+        "description": "We'd like to introduce the Nike React Ace Tour. It's the first performance golf shoe we've designed and refined with the female athlete in mind. Every detail, from the new traction pattern to the innovative lacing system, is built to perform, whether you're a professional or just starting your golf journey.",
+        "details": [
+            "2-year waterproof warranty",
+            "Colour Shown: White/Light Smoke Grey/Black",
+            "Style: CW3096-124",
+            "Country/Region of Origin: China"
+        ],
+        "rating": 4.6,
+        "images": "https://i.postimg.cc/XNLMdWR9/product21.png",
+        "price": 12495,
+        "color": "",
+        "shadow": ""
+    },
+    {
+        "_id": "6368ae4f5fc9689fed959540",
+        "type": "kid",
+        "title": "Nike Team Hustle D 10 FlyEase",
+        "text": "Younger Kids' Easy On/Off Shoes",
+        "description": "From playtime to getting buckets, the Nike Team Hustle D 10 is a slam dunk for players of all levels. With optimised traction on the sole, more secure lacing, plus a design that's durable and supportive, these are a major score whether you're shooting hoops or climbing on the playground.",
+        "details": [
+            "Soft foam cushioning",
+            "Durability when kids drag their toes.",
+            "Style: DD7302-012",
+            "Colour Shown: Black/Bright Spruce/Volt/Barely Volt",
+            "Country/Region of Origin: Indonesia"
+        ],
+        "rating": 4,
+        "images": "https://i.postimg.cc/VNnp6P9r/product22.png",
+        "price": 3995,
+        "color": "",
+        "shadow": ""
+    }
+]
+console.log(msg.length)
